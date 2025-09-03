aada29e4f8ba6d9eeb4a98620e37f5b246af162c
Ethan Barman <122973796+ethanhostspherein@users.noreply.github.com>
2025-09-03 22:57:25 +0530

    Create label.tsx

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+import { cn } from "@/lib/utils"
+// Label Component
+const Label = React.forwardRef
+  React.ElementRef<"label">,
+  React.ComponentPropsWithoutRef<"label">
+>(({ className, ...props }, ref) => (
+  <label
+    ref={ref}
+    className={cn(
+      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+      className
+    )}
+    {...props}
+  />
+))
+Label.displayName = "Label"
+
+export { Label }
